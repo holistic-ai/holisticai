3a9ced9af1a09072832894ec955db62cc78aace9
cristian <cristian.munoz@holisticai.com>
2023-04-21 12:01:43 -0300

    chore:pre-commit

--- holisticai/efficacy/metrics.py
+++ holisticai/efficacy/metrics.py
@@ -1,8 +1,9 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
 
 from ..utils._validation import _multiclass_checks
-from sklearn.metrics import mean_squared_error
+
 
 def confusion_matrix(y_pred, y_true, classes=None, normalize=None):
     """
@@ -102,44 +103,35 @@ def classification_efficacy_metrics(y_pred, y_true=None, y_proba=None):
         Metrics | Values | Reference
     """
     from sklearn import metrics
-    perform = \
-        {
-            'Accuracy': metrics.accuracy_score,
-            "Balanced accuracy": metrics.balanced_accuracy_score,
-            "Precision": metrics.precision_score,
-            "Recall": metrics.recall_score,
-            "F1-Score": metrics.f1_score
-        }
 
-    hyper = \
-        {
-            'Accuracy': {},
-            "Balanced accuracy": {},
-            "Precision": {},
-            "Recall": {},
-            "F1-Score": {}
-        }
+    perform = {
+        "Accuracy": metrics.accuracy_score,
+        "Balanced accuracy": metrics.balanced_accuracy_score,
+        "Precision": metrics.precision_score,
+        "Recall": metrics.recall_score,
+        "F1-Score": metrics.f1_score,
+    }
 
-    soft_perform = \
-        {
-            "AUC": metrics.roc_auc_score,
-            "Log Loss": metrics.log_loss
-        }
+    hyper = {
+        "Accuracy": {},
+        "Balanced accuracy": {},
+        "Precision": {},
+        "Recall": {},
+        "F1-Score": {},
+    }
 
-    soft_hyper = \
-        {
-            "AUC": {"average": "micro"},
-            "Log Loss": {}
-        }
-        
-    ref_vals ={
-            'Accuracy': 1,
-            "Balanced accuracy": 1,
-            "Precision": 1,
-            "Recall": 1,
-            "F1-Score": 1,
-            "AUC": 1,
-            "Log Loss": 0
+    soft_perform = {"AUC": metrics.roc_auc_score, "Log Loss": metrics.log_loss}
+
+    soft_hyper = {"AUC": {"average": "micro"}, "Log Loss": {}}
+
+    ref_vals = {
+        "Accuracy": 1,
+        "Balanced accuracy": 1,
+        "Precision": 1,
+        "Recall": 1,
+        "F1-Score": 1,
+        "AUC": 1,
+        "Log Loss": 0,
     }
     metrics = [
         [pf, fn(y_pred, y_true, **hyper[pf]), ref_vals[pf]]
@@ -151,10 +143,10 @@ def classification_efficacy_metrics(y_pred, y_true=None, y_proba=None):
             for pf, fn in soft_perform.items()
         ]
         metrics += opp_metrics
-    
-    return pd.DataFrame(
-        metrics, columns=["Metric", "Value", "Reference"]
-    ).set_index("Metric")
+
+    return pd.DataFrame(metrics, columns=["Metric", "Value", "Reference"]).set_index(
+        "Metric"
+    )
 
 
 def clustering_efficacy_metrics(X, y_pred):
@@ -182,6 +174,7 @@ def clustering_efficacy_metrics(X, y_pred):
     --------
     """
     from sklearn import metrics
+
     perform = {
         "Silhouette": metrics.silhouette_score,
         "Calinski Harabasz": metrics.calinski_harabasz_score,
@@ -189,31 +182,33 @@ def clustering_efficacy_metrics(X, y_pred):
     }
 
     hyper = {
-        "Silhouette": {"metric": 'euclidean'},
+        "Silhouette": {"metric": "euclidean"},
         "Calinski Harabasz": {},
         "Davies Bouldin": {},
     }
-        
-    ref_vals ={
-            'Silhouette': 1,
-            "Calinski Harabasz": np.inf,
-            "Davies Bouldin": 0,
+
+    ref_vals = {
+        "Silhouette": 1,
+        "Calinski Harabasz": np.inf,
+        "Davies Bouldin": 0,
     }
     metrics = [
-        [pf, fn(X, y_pred, **hyper[pf]), ref_vals[pf]]
-        for pf, fn in perform.items()
+        [pf, fn(X, y_pred, **hyper[pf]), ref_vals[pf]] for pf, fn in perform.items()
     ]
 
-    return pd.DataFrame(
-        metrics, columns=["Metric", "Value", "Reference"]
-    ).set_index("Metric")
+    return pd.DataFrame(metrics, columns=["Metric", "Value", "Reference"]).set_index(
+        "Metric"
+    )
 
 
 def is_numeric(obj):
-    attrs = ['__add__', '__sub__', '__mul__', '__truediv__', '__pow__']
+    attrs = ["__add__", "__sub__", "__mul__", "__truediv__", "__pow__"]
     return all(hasattr(obj, attr) for attr in attrs)
 
-def multiclassification_efficacy_metrics(y_pred, y_true=None, y_proba=None, classes=None, by_class=False):
+
+def multiclassification_efficacy_metrics(
+    y_pred, y_true=None, y_proba=None, classes=None, by_class=False
+):
     """
     Multiclassification efficacy metrics batch computation.
 
@@ -227,7 +222,7 @@ def multiclassification_efficacy_metrics(y_pred, y_true=None, y_proba=None, clas
     y_pred : array-like
         Predictions vector
     y_true (optional) : array-like
-        Target vector 
+        Target vector
     y_proba (optional) : array-like
         Probability Matrix estimates (num_examples, num_classes)
 
@@ -237,109 +232,121 @@ def multiclassification_efficacy_metrics(y_pred, y_true=None, y_proba=None, clas
         Metrics | Values | Reference
     """
     from sklearn import metrics
-    
+
     if classes is None:
         classes = sorted(np.unique(y_pred))
         if y_proba is not None:
-            assert all([is_numeric(c) for c in classes]), f"to evaluate y_proba outputs, parameter `classes` must be passed."
-        
+            assert all(
+                [is_numeric(c) for c in classes]
+            ), f"to evaluate y_proba outputs, parameter `classes` must be passed."
+
     def convert_to_numeric_array(y, classes):
-        if len(set(classes).intersection(set(np.unique(y))))==len(classes):
+        if len(set(classes).intersection(set(np.unique(y)))) == len(classes):
             y = pd.Series(list(y)).replace(dict(zip(classes, range(len(classes)))))
         return y
-    
+
     y_pred = convert_to_numeric_array(y_pred, classes)
     y_true = convert_to_numeric_array(y_true, classes)
-    
+
     n_classes = len(classes)
-        
-        
-    perform = \
-        {
-            'Accuracy': metrics.accuracy_score,
-            "Balanced accuracy": metrics.balanced_accuracy_score,
-            "Precision": metrics.precision_score,
-            "Recall": metrics.recall_score,
-            "F1-Score": metrics.f1_score
-        }
 
-    hyper = \
-        {
-            "Accuracy": {},
-            "Balanced accuracy": {},
-            "Precision": {"average": 'micro'},
-            "Recall": {"average": 'micro'},
-            "F1-Score": {"average": 'micro'},
-        }
-
-    soft_perform = \
-        {
-            "AUC": metrics.roc_auc_score,
-            "Log Loss": metrics.log_loss
-        }
-
-    soft_hyper = \
-            {
-                "AUC": {"average": "macro", "multi_class": "ovr"},
-                "Log Loss": {},
-            }
-        
-    ref_vals ={
-            'Accuracy': 1,
-            "Balanced accuracy": 1,
-            "Precision": 1,
-            "Recall": 1,
-            "F1-Score": 1,
-            "AUC": 1,
-            "Log Loss": 0
+    perform = {
+        "Accuracy": metrics.accuracy_score,
+        "Balanced accuracy": metrics.balanced_accuracy_score,
+        "Precision": metrics.precision_score,
+        "Recall": metrics.recall_score,
+        "F1-Score": metrics.f1_score,
     }
-    
-    detailed_hyper ={
-                    "Precision": {"average": None},
-                    "Recall": {"average": None},
-                    "F1-Score": {"average": None},
-                    "AUC": {"average": None, "multi_class": "ovr"},
-                    }
-    
+
+    hyper = {
+        "Accuracy": {},
+        "Balanced accuracy": {},
+        "Precision": {"average": "micro"},
+        "Recall": {"average": "micro"},
+        "F1-Score": {"average": "micro"},
+    }
+
+    soft_perform = {"AUC": metrics.roc_auc_score, "Log Loss": metrics.log_loss}
+
+    soft_hyper = {
+        "AUC": {"average": "macro", "multi_class": "ovr"},
+        "Log Loss": {},
+    }
+
+    ref_vals = {
+        "Accuracy": 1,
+        "Balanced accuracy": 1,
+        "Precision": 1,
+        "Recall": 1,
+        "F1-Score": 1,
+        "AUC": 1,
+        "Log Loss": 0,
+    }
+
+    detailed_hyper = {
+        "Precision": {"average": None},
+        "Recall": {"average": None},
+        "F1-Score": {"average": None},
+        "AUC": {"average": None, "multi_class": "ovr"},
+    }
+
     def process_value(metrics_dict, hyper_dict, y_pred, y_true):
         values_list = []
         for pf, fn in metrics_dict.items():
             result = fn(y_true, y_pred, **hyper_dict[pf])
             values_list.append(result)
         return values_list
-        
-    def process_metrics(values_list, metrics_dict, y_pred, y_true_oh, n_classes, detailed_hyper):
+
+    def process_metrics(
+        values_list, metrics_dict, y_pred, y_true_oh, n_classes, detailed_hyper
+    ):
         metrics_list = []
-        for value, (pf, fn) in zip(values_list,metrics_dict.items()):
+        for value, (pf, fn) in zip(values_list, metrics_dict.items()):
             result = [value]
             if pf in detailed_hyper:
                 result += list(fn(y_true_oh, y_pred, **detailed_hyper[pf]))
             else:
-                result += [""]*n_classes
+                result += [""] * n_classes
             metrics_list.append(result)
         return metrics_list
 
     metrics_1 = process_value(perform, hyper, y_pred, y_true)
     if by_class:
-        metrics_1 = process_metrics(metrics_1, perform, y_pred, y_true, n_classes, detailed_hyper)   
-        metrics = [[pf] + metric + [ref_vals[pf]] for metric,(pf,fn) in zip(metrics_1,perform.items())]
+        metrics_1 = process_metrics(
+            metrics_1, perform, y_pred, y_true, n_classes, detailed_hyper
+        )
+        metrics = [
+            [pf] + metric + [ref_vals[pf]]
+            for metric, (pf, fn) in zip(metrics_1, perform.items())
+        ]
     else:
-        metrics = [[pf] + [metric] + [ref_vals[pf]] for metric,(pf,fn) in zip(metrics_1,perform.items())]
-    
+        metrics = [
+            [pf] + [metric] + [ref_vals[pf]]
+            for metric, (pf, fn) in zip(metrics_1, perform.items())
+        ]
+
     if y_proba is not None:
         y_true_oh = y_true.replace(dict(zip(classes, range(len(classes)))))
         metrics_2 = process_value(soft_perform, soft_hyper, y_proba, y_true_oh)
         if by_class:
-            metrics_2 = process_metrics(metrics_2, soft_perform, y_proba, y_true_oh, n_classes, detailed_hyper)   
-            metrics_2 = [[pf] + metric + [ref_vals[pf]] for metric,(pf,fn) in zip(metrics_2,soft_perform.items())]
+            metrics_2 = process_metrics(
+                metrics_2, soft_perform, y_proba, y_true_oh, n_classes, detailed_hyper
+            )
+            metrics_2 = [
+                [pf] + metric + [ref_vals[pf]]
+                for metric, (pf, fn) in zip(metrics_2, soft_perform.items())
+            ]
         else:
-            metrics_2 = [[pf] + [metric] + [ref_vals[pf]] for metric,(pf,fn) in zip(metrics_2,soft_perform.items())]
+            metrics_2 = [
+                [pf] + [metric] + [ref_vals[pf]]
+                for metric, (pf, fn) in zip(metrics_2, soft_perform.items())
+            ]
         metrics += metrics_2
-    
+
     values_cols = ["Value"]
     if by_class:
         values_cols += classes
-    
+
     return pd.DataFrame(
         metrics, columns=["Metric"] + values_cols + ["Reference"]
     ).set_index("Metric")
@@ -347,6 +354,7 @@ def multiclassification_efficacy_metrics(y_pred, y_true=None, y_proba=None, clas
 
 def rmse_score(y_true, y_pred, **kargs):
     return np.sqrt(mean_squared_error(y_true, y_pred, **kargs))
+
 
 def regression_efficacy_metrics(y_pred, y_true):
     """
@@ -362,7 +370,7 @@ def regression_efficacy_metrics(y_pred, y_true):
     y_pred : array-like
         Predictions vector
     y_true (optional) : array-like
-        Target vector 
+        Target vector
 
     Returns
     -------
@@ -370,7 +378,7 @@ def regression_efficacy_metrics(y_pred, y_true):
         Metrics | Values | Reference
     """
     from sklearn import metrics
-    
+
     perform = {
         # "Pseudo-R2": pr2_score,
         "RMSE": rmse_score,
@@ -381,27 +389,24 @@ def regression_efficacy_metrics(y_pred, y_true):
 
     hyper = {
         # "Pseudo-R2": {},
-        "RMSE": {"sample_weight": None,
-                    "multioutput": 'uniform_average'},
-        "MAE": {"sample_weight": None,
-                "multioutput": 'uniform_average'},
-        "MAPE": {"sample_weight": None,
-                    "multioutput": 'uniform_average'},
+        "RMSE": {"sample_weight": None, "multioutput": "uniform_average"},
+        "MAE": {"sample_weight": None, "multioutput": "uniform_average"},
+        "MAPE": {"sample_weight": None, "multioutput": "uniform_average"},
         "Max Error": {},
     }
-    
-    ref_vals ={
-        'RMSE': 0,
+
+    ref_vals = {
+        "RMSE": 0,
         "MAE": 0,
         "MAPE": 0,
         "Max Error": 0,
     }
-        
+
     metrics = [
         [pf, fn(y_pred, y_true, **hyper[pf]), ref_vals[pf]]
         for pf, fn in perform.items()
     ]
-    
-    return pd.DataFrame(
-        metrics, columns=["Metric", "Value", "Reference"]
-    ).set_index("Metric")
+
+    return pd.DataFrame(metrics, columns=["Metric", "Value", "Reference"]).set_index(
+        "Metric"
+    )
