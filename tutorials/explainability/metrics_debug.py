d8347376e6f90323465f77a6614e315fba31b906
cristian.munoz <cristian.munoz@holisticai.com>
2023-09-01 00:44:51 +0000

    chore: pre-commit

--- tutorials/explainability/metrics_debug.py
+++ tutorials/explainability/metrics_debug.py
@@ -1,25 +1,29 @@
 import sys
+
 sys.path.append("./")
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 import pandas as pd
-from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
 from holisticai.datasets import load_adult
 
 # data and simple preprocessing
-dataset = load_adult()['frame']
-dataset = dataset.iloc[0:1000,]
-X = pd.get_dummies(dataset.drop(columns=['class', 'fnlwgt']), drop_first=True)
+dataset = load_adult()["frame"]
+dataset = dataset.iloc[
+    0:1000,
+]
+X = pd.get_dummies(dataset.drop(columns=["class", "fnlwgt"]), drop_first=True)
 scaler = StandardScaler()
 X_standard = scaler.fit_transform(X)
 X_standard = pd.DataFrame(X_standard, columns=X.columns)
 
-y_clf = pd.DataFrame(dataset['class'].apply(lambda x: 1 if x == '>50K' else 0))
-y_reg = pd.DataFrame(dataset['fnlwgt'])
+y_clf = pd.DataFrame(dataset["class"].apply(lambda x: 1 if x == ">50K" else 0))
+y_reg = pd.DataFrame(dataset["fnlwgt"])
 y_reg = scaler.fit_transform(y_reg)
 
 # regression
@@ -34,11 +38,13 @@ clf.fit(X_standard, y_clf)
 from holisticai.explainability import Explainer
 
 # instantiate explainer permutation
-explainer = Explainer(based_on='feature_importance', 
-                      strategy_type='surrogate', 
-                      model_type='regression', 
-                      model=reg, 
-                      x=X_standard, 
-                      y=y_reg)
+explainer = Explainer(
+    based_on="feature_importance",
+    strategy_type="surrogate",
+    model_type="regression",
+    model=reg,
+    x=X_standard,
+    y=y_reg,
+)
 
 print(explainer.metrics())
