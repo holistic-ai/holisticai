9e0fd4f8795aa4c4f73f618090a978c4f1c18aca
Kleyt0n <kleyton.vsc@gmail.com>
2023-09-13 23:32:30 -0300

    fix: pre commit

--- tutorials/explainability/paper_explainability.py
+++ tutorials/explainability/paper_explainability.py
@@ -1,38 +1,50 @@
-import pandas as pd
-import numpy as np
+import os
 
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
-from sklearn.ensemble import RandomForestClassifier
 
 from holisticai.explainability import Explainer
 
-import os 
 os.chdir("tutorials/explainability")
 
-X = pd.read_csv('data/X_orig.csv', index_col=0)
-#X = X.iloc[:200, :]
-y = np.load('data/y_orig.npy')
-#y = y[:200]
+X = pd.read_csv("data/X_orig.csv", index_col=0)
+# X = X.iloc[:200, :]
+y = np.load("data/y_orig.npy")
+# y = y[:200]
 
 # clean up a bit
 for c in X.columns:
     if c.endswith("_isBlank"):
-        del X[c]   
-X["bmi"] = 10000 * X["weight"].values.copy() / (X["height"].values.copy() * X["height"].values.copy())
+        del X[c]
+X["bmi"] = (
+    10000
+    * X["weight"].values.copy()
+    / (X["height"].values.copy() * X["height"].values.copy())
+)
 del X["weight"]
 del X["height"]
-del X["urine_hematest_isTrace"] # would have no variance in the strain set
-del X["SGOT_isBlankbutapplicable"] # would have no variance in the strain set
-del X["calcium_isBlankbutapplicable"] # would have no variance in the strain set
-del X["uric_acid_isBlankbutapplicable"] # would only have one true value in the train set
-del X["urine_hematest_isVerylarge"] # would only have one true value in the train set
-del X["total_bilirubin_isBlankbutapplicable"] # would only have one true value in the train set
-del X["alkaline_phosphatase_isBlankbutapplicable"] # would only have one true value in the train set
-del X["hemoglobin_isUnacceptable"] # redundant with hematocrit_isUnacceptable
-rows = np.where(np.invert(np.isnan(X["systolic_blood_pressure"]) | np.isnan(X["bmi"])))[0]
-X = X.iloc[rows,:]
+del X["urine_hematest_isTrace"]  # would have no variance in the strain set
+del X["SGOT_isBlankbutapplicable"]  # would have no variance in the strain set
+del X["calcium_isBlankbutapplicable"]  # would have no variance in the strain set
+del X[
+    "uric_acid_isBlankbutapplicable"
+]  # would only have one true value in the train set
+del X["urine_hematest_isVerylarge"]  # would only have one true value in the train set
+del X[
+    "total_bilirubin_isBlankbutapplicable"
+]  # would only have one true value in the train set
+del X[
+    "alkaline_phosphatase_isBlankbutapplicable"
+]  # would only have one true value in the train set
+del X["hemoglobin_isUnacceptable"]  # redundant with hematocrit_isUnacceptable
+rows = np.where(np.invert(np.isnan(X["systolic_blood_pressure"]) | np.isnan(X["bmi"])))[
+    0
+]
+X = X.iloc[rows, :]
 y = y[rows]
 
 name_map = {
@@ -40,7 +52,7 @@ name_map = {
     "age": "Age",
     "systolic_blood_pressure": "Systolic blood pressure",
     "bmi": "BMI",
-    "white_blood_cells": "White blood cells", # (mg/dL)
+    "white_blood_cells": "White blood cells",  # (mg/dL)
     "sedimentation_rate": "Sedimentation rate",
     "serum_albumin": "Blood albumin",
     "alkaline_phosphatase": "Alkaline phosphatase",
@@ -50,14 +62,14 @@ name_map = {
     "uric_acid": "Uric acid",
     "red_blood_cells": "Red blood cells",
     "urine_albumin_isNegative": "Albumin present in urine",
-    "serum_protein": "Blood protein"
+    "serum_protein": "Blood protein",
 }
 mapped_feature_names = list(map(lambda x: name_map.get(x, x), X.columns))
 
 # split by patient id
 pids = np.unique(X.index.values)
-train_pids,test_pids = train_test_split(pids, random_state=0)
-strain_pids,valid_pids = train_test_split(train_pids, random_state=0)
+train_pids, test_pids = train_test_split(pids, random_state=0)
+strain_pids, valid_pids = train_test_split(train_pids, random_state=0)
 
 # find the indexes of the samples from the patient ids
 train_inds = np.where([p in train_pids for p in X.index.values])[0]
@@ -66,10 +78,10 @@ valid_inds = np.where([p in valid_pids for p in X.index.values])[0]
 test_inds = np.where([p in test_pids for p in X.index.values])[0]
 
 # create the split datasets
-X_train = X.iloc[train_inds,:]
-X_strain = X.iloc[strain_inds,:]
-X_valid = X.iloc[valid_inds,:]
-X_test = X.iloc[test_inds,:]
+X_train = X.iloc[train_inds, :]
+X_strain = X.iloc[strain_inds, :]
+X_valid = X.iloc[valid_inds, :]
+X_test = X.iloc[test_inds, :]
 y_train = y[train_inds]
 y_strain = y[strain_inds]
 y_valid = y[valid_inds]
@@ -94,20 +106,23 @@ X_test_imp = scaler.transform(X_test_imp)
 X_imp = scaler.transform(X_imp)
 
 import numpy as np
-seed = np.random.seed(42) # set seed for reproducibility
+
+seed = np.random.seed(42)  # set seed for reproducibility
 # simple preprocessing
 
-model = RandomForestClassifier() # instantiate model
-model.fit(X_train_imp, y_train) # fit model
+model = RandomForestClassifier()  # instantiate model
+model.fit(X_train_imp, y_train)  # fit model
 
-y_pred = model.predict(X_test_imp) # compute predictions
+y_pred = model.predict(X_test_imp)  # compute predictions
 
 # permutation feature importance
-explainer = Explainer(based_on='feature_importance',
-                      strategy_type='permutation',
-                      model_type='binary_classification',
-                      model = model, 
-                      x = X_train_imp, 
-                      y = y_train)
+explainer = Explainer(
+    based_on="feature_importance",
+    strategy_type="permutation",
+    model_type="binary_classification",
+    model=model,
+    x=X_train_imp,
+    y=y_train,
+)
 
-explainer.metrics() # compute metrics
+explainer.metrics()  # compute metrics
