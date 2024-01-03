190cc7ffcaf573ad700fa65fc59ee7f152d414e6
Kleyt0n <kleyton.vsc@gmail.com>
2024-01-03 19:00:09 -0300

    fix: organize tutorials and add explainability paper tests

--- tutorials/explainability/explainability_debug.py
+++ tutorials/explainability/explainability_debug.py
@@ -1,13 +1,16 @@
 import sys
-sys.path.append('./')
+
+sys.path.append("./")
 
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+
 from tutorials.explainability.paper_metrics.utils import load_processed_adult
+
 seed = np.random.seed(42)
 
 X_train, X_test, y_train, y_test = load_processed_adult(seed=seed)
-model_type='binary_classification'
+model_type = "binary_classification"
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
@@ -15,11 +18,14 @@ y_pred = model.predict(X_test)
 
 # permutation feature importance
 from holisticai.explainability import Explainer
-explainer = Explainer(based_on='feature_importance',
-                      strategy_type='permutation',
-                      model_type='binary_classification',
-                      model = model, 
-                      x = X_test, 
-                      y = y_pred)
+
+explainer = Explainer(
+    based_on="feature_importance",
+    strategy_type="permutation",
+    model_type="binary_classification",
+    model=model,
+    x=X_test,
+    y=y_pred,
+)
 
 explainer.bar_plot(max_display=10)
