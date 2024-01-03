45cf4dc7db27ee4d71eea951b46dfe1542cfe3e7
Kleyt0n <kleyton.vsc@gmail.com>
2024-01-03 18:52:38 -0300

    fix: organize tutorials and add explainability paper tests

--- tutorials/explainability/paper_metrics/test.py
+++ tutorials/explainability/paper_metrics/test.py
@@ -1,13 +1,24 @@
 import sys
-sys.path.append('../../')
+
+sys.path.append("../../")
 
 import numpy as np
-from tutorials.explainability.paper_metrics.utils import load_processed_adult , train_classifier_model, train_regression_model, load_processed_diabetes
-from tutorials.explainability.paper_metrics.utils import run_permutation_explainability, run_surrogate_explainability, run_shap_explainability, run_lime_explainability
+
+from tutorials.explainability.paper_metrics.utils import (
+    load_processed_adult,
+    load_processed_diabetes,
+    run_lime_explainability,
+    run_permutation_explainability,
+    run_shap_explainability,
+    run_surrogate_explainability,
+    train_classifier_model,
+    train_regression_model,
+)
+
 seed = np.random.seed(42)
 
 X_train, X_test, y_train, y_test = load_processed_diabetes(seed=seed)
-model_type='regression'
+model_type = "regression"
 outputs = train_regression_model(X_train, X_test, y_train, y_test)
 
 print("Permutation")
@@ -21,7 +32,7 @@ run_lime_explainability(model_type, outputs)
 
 
 X_train, X_test, y_train, y_test = load_processed_adult(seed=seed)
-model_type='binary_classification'
+model_type = "binary_classification"
 outputs = train_classifier_model(X_train, X_test, y_train, y_test)
 print("Permutation")
 run_permutation_explainability(model_type, outputs)
@@ -31,5 +42,3 @@ print("Shap")
 run_shap_explainability(model_type, outputs)
 print("Lime")
 run_lime_explainability(model_type, outputs)
-
-
