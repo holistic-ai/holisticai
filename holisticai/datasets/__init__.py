e48e2376f97b9a2cce256a6907950b2480009c76
Franklin <44931423+fracarfer5@users.noreply.github.com>
2024-01-04 12:22:06 -0300

    Refactoring tutorial datasets (#96)
    * refactor: refactoring tutorial datasets
    
    * refactor: adding kargs in functions
    
    * refactor: updating function parameters and adding private functions
    
    * chore: updating function name
    
    * docs: updating docstrings for datasets preprocessing description
    
    * refactor: updating binary classification tutorials with new datasets
    
    * refactor: updating binary classification tutorials with new datasets
    
    * refactor: adding dataframe and array option to lastdf processing function
    
    * refactor: updating recsys tutorials with new dataset processing
    
    * feat: adding heart dataset to dataloader
    
    * refactor: updating clustering tutorials with new dataset processing
    
    * refactor: updating multi-class tutorials with new dataset processing
    
    * fix: removing unused graph
    
    * refactor: updating bias measuring tutorials with new dataset processing
    
    ---------
    
    Co-authored-by: Franklin Fernandez <franklincf@tecgraf.puc-rio.br>

--- holisticai/datasets/__init__.py
+++ holisticai/datasets/__init__.py
@@ -4,11 +4,13 @@ The :mod:`holisticai.datasets` module includes dataloaders for quick experimenta
 
 from ._dataloaders import (
     load_adult,
+    load_heart,
     load_last_fm,
     load_law_school,
     load_student,
     load_us_crime,
 )
+from ._preprocessers import load_dataset
 
 __all__ = [
     "load_adult",
@@ -16,4 +18,6 @@ __all__ = [
     "load_law_school",
     "load_last_fm",
     "load_us_crime",
+    "load_heart",
+    "load_dataset",
 ]
