975d84bb047a57b9370c7d25faaf35ea09ea00dc
Kleyton da Costa <44351707+Kleyt0n@users.noreply.github.com>
2024-01-04 15:19:05 -0300

    fix: explainability ease score and sort feat imp (#97)
    * fix: explainability ease score and sort feat imp
    
    * fix: explainability ease score and sort feat imp
    
    * chore: update codes and create run experiments script for paper
    
    * fix: organize tutorials and add explainability paper tests
    
    * fix: organize tutorials and add explainability paper tests
    
    * fix: organize tutorials and add explainability paper tests
    
    * fix: plots color and functionality
    
    * fix: important similarity metric
    
    * fix: remove small test explainability
    
    ---------
    
    Co-authored-by: crismunoz <crisstrink@gmail.com>
    Co-authored-by: cristian.munoz <cristian.munoz@holisticai.com>

--- holisticai/explainability/metrics/utils/feature_importance.py
+++ holisticai/explainability/metrics/utils/feature_importance.py
@@ -2,7 +2,7 @@ from ..utils import alpha_feature_importance
 
 
 class GlobalFeatureImportance:
-    def get_alpha_feature_importance(self, alpha):
+    def get_alpha_feature_importance(self, alpha=0.8):
 
         feat_imp = alpha_feat_imp = self.feature_importance.set_index(
             "Variable"
@@ -12,30 +12,47 @@ class GlobalFeatureImportance:
             alpha_feat_imp = alpha_feature_importance(feat_imp, alpha)
 
         alpha_cond_feat_imp = None
+        cond_feat_imp = None
         if hasattr(self, "conditional_feature_importance") and (
             self.conditional_feature_importance is not None
         ):
-            alpha_cond_feat_imp = {
-                label: value.set_index("Variable")
-                .sort_values("Importance", ascending=False)
-                .iloc[: len(alpha_feat_imp.index)]
+            cond_feat_imp = {
+                label: value.set_index("Variable").sort_values(
+                    "Importance", ascending=False
+                )
                 for label, value in self.conditional_feature_importance.items()
             }
 
-        return feat_imp, (alpha_feat_imp, alpha_cond_feat_imp)
+            alpha_cond_feat_imp = {
+                label: alpha_feature_importance(value, alpha)
+                for label, value in self.conditional_feature_importance.items()
+            }
 
-    def partial_dependence_plot(self, first=0, last=None, **plot_kargs):
+        return (feat_imp, cond_feat_imp), (alpha_feat_imp, alpha_cond_feat_imp)
+
+    def partial_dependence_plot(self, first=0, last=None, alpha=0.8, **plot_kargs):
 
         import matplotlib.pyplot as plt
+        import numpy as np
+        import seaborn as sns
         from sklearn.inspection import PartialDependenceDisplay
 
-        top_k = None
+        sns.set_style("whitegrid")
+
         if last == None:
             last = first + 6
 
-        importances = self.get_alpha_feature_importance(alpha=top_k)
-        feat_imp, (alpha_feat_imp, alpha_cond_feat_imp) = importances
+        importances = self.get_alpha_feature_importance(alpha)
+        (feat_imp, _), (alpha_feat_imp, alpha_cond_feat_imp) = importances
+
+        from ..global_importance import ExplainabilityEase
+
+        expe = ExplainabilityEase(
+            model_type=self.model_type, model=self.model, x=self.x
+        )
+        _, score_data = expe(alpha_feat_imp, return_score_data=True)
         features = list(alpha_feat_imp.index)[first:last]
+        level = [score_data.loc[f]["scores"] for f in features]
         title = "Partial dependence plot"
         percentiles = (
             (0, 1) if self.model_type == "binary_classification" else (0.05, 0.95)
@@ -44,7 +61,7 @@ class GlobalFeatureImportance:
         common_params = {
             "subsample": 50,
             "n_jobs": 2,
-            "grid_resolution": 20,
+            "grid_resolution": 50,
             "random_state": 0,
             "kind": "average",
             "percentiles": percentiles,
@@ -61,7 +78,18 @@ class GlobalFeatureImportance:
             features,
             **common_params,
         )
+
+        for lv, ax in zip(level, pdp.axes_[0]):
+            ax.legend([lv])
+
+        acc_feat_imp = np.sum(alpha_feat_imp["Importance"])
+        num_features = len(alpha_feat_imp)
+        print(
+            f"The accumulated feature importance for {num_features} features is equal {acc_feat_imp:.4f} < {alpha}."
+        )
         pdp.figure_.suptitle(title)
+
+        return pdp
 
 
 class LocalFeatureImportance:
