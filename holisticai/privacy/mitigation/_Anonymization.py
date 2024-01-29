4f599ae22fe97bba4db975b499dabbd3d6fa6397
aminatkhamokova <amina.tkhamokova@holisticai.com>
2024-01-29 14:40:02 +0000

    privacy.anonymization added with test

--- holisticai/privacy/mitigation/_Anonymization.py
+++ holisticai/privacy/mitigation/_Anonymization.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-#Copyright (c) 2021 International Business Machines
+# Copyright (c) 2021 International Business Machines
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -17,25 +17,25 @@
 # SOFTWARE.
 
 
-#The 'Anonymize' class was taken from the AI Privacy Toolkit github repository, with certain modifications applied.
+# The 'Anonymize' class was taken from the AI Privacy Toolkit github repository, with certain modifications applied.
 
 
 from collections import Counter
+from typing import Optional, Union
 
+import numpy as np
+import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.preprocessing import OneHotEncoder
-from typing import Union, Optional
-import pandas as pd
-import numpy as np
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
 
 class Anonymize:
     """
     Class for performing tailored, model-guided anonymization of training datasets for ML models.
-    
+
     References:
 
     Goldsteen, Abigail, Gilad Ezov, Ron Shmelkin, Micha Moffie, and Ariel Farkash. "Anonymizing machine learning models."
@@ -43,15 +43,17 @@ class Anonymize:
 
     """
 
-    def __init__(self,
-                 k: int,
-                 quasi_identifiers: Union[np.ndarray, list],
-                 features: Optional[list] = None,
-                 features_names: Optional[list] = None,
-                 quasi_identifer_slices: Optional[list] = None,
-                 categorical_features: Optional[list] = None,
-                 is_regression: Optional[bool] = False,
-                 train_only_QI: Optional[bool] = False):
+    def __init__(
+        self,
+        k: int,
+        quasi_identifiers: Union[np.ndarray, list],
+        features: Optional[list] = None,
+        features_names: Optional[list] = None,
+        quasi_identifer_slices: Optional[list] = None,
+        categorical_features: Optional[list] = None,
+        is_regression: Optional[bool] = False,
+        train_only_QI: Optional[bool] = False,
+    ):
         """
         Parameters
         ----------
@@ -85,7 +87,9 @@ class Anonymize:
         self
         """
         if k < 2:
-            raise ValueError("k should be a positive integer with a value of 2 or higher")
+            raise ValueError(
+                "k should be a positive integer with a value of 2 or higher"
+            )
         if quasi_identifiers is None or len(quasi_identifiers) < 1:
             raise ValueError("The list of quasi-identifiers cannot be empty")
 
@@ -105,7 +109,7 @@ class Anonymize:
         Method for performing model-guided anonymization.
 
         Parameters
-        ----------       
+        ----------
         X_train : np.ndarray or pandas DataFrame
             Dataset containing the training data for the model.
 
@@ -115,26 +119,34 @@ class Anonymize:
         Returns
         -------
         The anonymized training dataset as pandas DataFrame.
-        
-        """        
+
+        """
         if X_train.shape[1] != 0:
             self.features = [i for i in range(X_train.shape[1])]
         else:
-            raise ValueError('No data provided')
+            raise ValueError("No data provided")
 
         if self.features_names is None:
             # if no names provided, use numbers instead
             self.features_names = self.features
 
         if not set(self.quasi_identifiers).issubset(set(self.features_names)):
-            raise ValueError('Quasi identifiers should bs a subset of the supplied features or indexes in range of '
-                             'the data columns')
-        if self.categorical_features and not set(self.categorical_features).issubset(set(self.features_names)):
-            raise ValueError('Categorical features should bs a subset of the supplied features or indexes in range of '
-                             'the data columns')
-   
+            raise ValueError(
+                "Quasi identifiers should bs a subset of the supplied features or indexes in range of "
+                "the data columns"
+            )
+        if self.categorical_features and not set(self.categorical_features).issubset(
+            set(self.features_names)
+        ):
+            raise ValueError(
+                "Categorical features should bs a subset of the supplied features or indexes in range of "
+                "the data columns"
+            )
+
         # transform quasi identifiers to indexes
-        self.quasi_identifiers = [i for i, v in enumerate(self.features_names) if v in self.quasi_identifiers]
+        self.quasi_identifiers = [
+            i for i, v in enumerate(self.features_names) if v in self.quasi_identifiers
+        ]
         if self.quasi_identifer_slices:
             temp_list = []
             for slice in self.quasi_identifer_slices:
@@ -142,18 +154,23 @@ class Anonymize:
                 temp_list.append(new_slice)
             self.quasi_identifer_slices = temp_list
         if self.categorical_features:
-            self.categorical_features = [i for i, v in enumerate(self.features_names) if v in self.categorical_features]
+            self.categorical_features = [
+                i
+                for i, v in enumerate(self.features_names)
+                if v in self.categorical_features
+            ]
 
         transformed = self._anonymize(X_train.copy(), y_train)
         return pd.DataFrame(transformed, columns=self.features_names)
 
-
     def _anonymize(self, x, y):
         if x.shape[0] != y.shape[0]:
             raise ValueError("x and y should have same number of rows")
-        if any(x.select_dtypes(include='category').columns):
+        if any(x.select_dtypes(include="category").columns):
             if not self.categorical_features:
-                raise ValueError('when supplying an array with non-numeric data, categorical_features must be defined')
+                raise ValueError(
+                    "when supplying an array with non-numeric data, categorical_features must be defined"
+                )
             x_prepared = self._modify_categorical_features(x)
         else:
             x_prepared = x
@@ -162,9 +179,13 @@ class Anonymize:
             # build DT just on QI features
             x_anonymizer_train = x_prepared[:, self.quasi_identifiers]
         if self.is_regression:
-            self._anonymizer = DecisionTreeRegressor(random_state=10, min_samples_split=2, min_samples_leaf=self.k)
+            self._anonymizer = DecisionTreeRegressor(
+                random_state=10, min_samples_split=2, min_samples_leaf=self.k
+            )
         else:
-            self._anonymizer = DecisionTreeClassifier(random_state=10, min_samples_split=2, min_samples_leaf=self.k)
+            self._anonymizer = DecisionTreeClassifier(
+                random_state=10, min_samples_split=2, min_samples_leaf=self.k
+            )
 
         self._anonymizer.fit(x_anonymizer_train, y)
         cells_by_id = self._calculate_cells(x, x_anonymizer_train)
@@ -181,8 +202,8 @@ class Anonymize:
                 # TODO we may change the method for choosing representative for cell
                 # label_hist = self.anonymizer.tree_.value[node][0]
                 # label = int(self.anonymizer.classes_[np.argmax(label_hist)])
-                cell = {'label': 1, 'hist': hist, 'id': int(node)}
-                cells_by_id[cell['id']] = cell
+                cell = {"label": 1, "hist": hist, "id": int(node)}
+                cells_by_id[cell["id"]] = cell
         self._nodes = leaves
         self._find_representatives(x, x_anonymizer_train, cells_by_id.values())
         return cells_by_id
@@ -191,17 +212,27 @@ class Anonymize:
         # x is original data (always numpy), x_anonymizer_train is only QIs + 1-hot encoded
         node_ids = self._find_sample_nodes(x_anonymizer_train)
         if self.quasi_identifer_slices:
-            all_one_hot_features = set([feature for encoded in self.quasi_identifer_slices for feature in encoded])
+            all_one_hot_features = set(
+                [
+                    feature
+                    for encoded in self.quasi_identifer_slices
+                    for feature in encoded
+                ]
+            )
         else:
             all_one_hot_features = set()
         for cell in cells:
-            cell['representative'] = {}
+            cell["representative"] = {}
             # get all rows in cell
-            indexes = [index for index, node_id in enumerate(node_ids) if node_id == cell['id']]
+            indexes = [
+                index for index, node_id in enumerate(node_ids) if node_id == cell["id"]
+            ]
             # TODO: should we filter only those with majority label? (using hist)
             rows = x.loc[indexes]
             done = set()
-            for feature in self.quasi_identifiers:  # self.quasi_identifiers are numerical indexes
+            for (
+                feature
+            ) in self.quasi_identifiers:  # self.quasi_identifiers are numerical indexes
                 if feature not in done:
                     # deal with 1-hot encoded features
                     if feature in all_one_hot_features:
@@ -209,16 +240,23 @@ class Anonymize:
                         for encoded in self.quasi_identifer_slices:
                             if feature in encoded:
                                 values = rows[:, encoded]
-                                unique_rows, counts = np.unique(values, axis=0, return_counts=True)
+                                unique_rows, counts = np.unique(
+                                    values, axis=0, return_counts=True
+                                )
                                 rep = unique_rows[np.argmax(counts)]
                                 for i, e in enumerate(encoded):
                                     done.add(e)
-                                    cell['representative'][e] = rep[i]
+                                    cell["representative"][e] = rep[i]
                     else:  # rest of features
                         values = rows.iloc[:, feature]
-                        if self.categorical_features and feature in self.categorical_features:
+                        if (
+                            self.categorical_features
+                            and feature in self.categorical_features
+                        ):
                             # find most common value
-                            cell['representative'][feature] = Counter(values).most_common(1)[0][0]
+                            cell["representative"][feature] = Counter(
+                                values
+                            ).most_common(1)[0][0]
                         else:
                             # find the mean value (per feature)
                             median = np.median(values)
@@ -230,12 +268,15 @@ class Anonymize:
                                 if dist < min_dist:
                                     min_dist = dist
                                     min_value = value
-                            cell['representative'][feature] = min_value
+                            cell["representative"][feature] = min_value
 
     def _find_sample_nodes(self, samples):
         paths = self._anonymizer.decision_path(samples).toarray()
         node_set = set(self._nodes)
-        return [(list(set([i for i, v in enumerate(p) if v == 1]) & node_set))[0] for p in paths]
+        return [
+            (list(set([i for i, v in enumerate(p) if v == 1]) & node_set))[0]
+            for p in paths
+        ]
 
     def _find_sample_cells(self, samples, cells_by_id):
         node_ids = self._find_sample_nodes(samples)
@@ -248,8 +289,8 @@ class Anonymize:
         for ii, row in x.iterrows():
             cell = cells[index]
             index += 1
-            for feature in cell['representative']:
-                row[feature] = cell['representative'][feature]
+            for feature in cell["representative"]:
+                row[feature] = cell["representative"][feature]
             dfnew.loc[len(dfnew)] = row
         return dfnew
 
@@ -258,10 +299,16 @@ class Anonymize:
         used_features = self.features
         if self.train_only_QI:
             used_features = self.quasi_identifiers
-        numeric_features = [f for f in self.features if f in used_features and f not in self.categorical_features]
-        categorical_features = [f for f in self.categorical_features if f in used_features]
+        numeric_features = [
+            f
+            for f in self.features
+            if f in used_features and f not in self.categorical_features
+        ]
+        categorical_features = [
+            f for f in self.categorical_features if f in used_features
+        ]
         numeric_transformer = Pipeline(
-            steps=[('imputer', SimpleImputer(strategy='constant', fill_value=0))]
+            steps=[("imputer", SimpleImputer(strategy="constant", fill_value=0))]
         )
 
         categorical_transformer = OneHotEncoder(handle_unknown="ignore", sparse=False)
