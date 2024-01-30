a012abc6e12e0d91191c685883566b6836add19e
aminatkhamokova <amina.tkhamokova@holisticai.com>
2024-01-30 15:44:36 +0000

    reference changed

--- holisticai/privacy/mitigation/_Anonymization.py
+++ holisticai/privacy/mitigation/_Anonymization.py
@@ -1,24 +1,9 @@
-# MIT License
-#
-# Copyright (c) 2021 International Business Machines
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-# persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
-# Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+#AI privacy toolkit
+#Abigail Goldsteen, Ola Saadi, Ron Shmelkin, Shlomit Shachor, Natalia Razinkov, "AI privacy toolkit", SoftwareX, Volume 22, 2023, 101352, ISSN 2352-7110
 
+#Retrieved from https://www.softxjournal.com/article/S2352-7110(23)00048-1/fulltext
 
-# The 'Anonymize' class was taken from the AI Privacy Toolkit github repository, with certain modifications applied.
-
+#GitHub Repository: https://github.com/IBM/ai-privacy-toolkit
 
 from collections import Counter
 from typing import Optional, Union
