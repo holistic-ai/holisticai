d94ed4145eac3b2e93f37673b8ab9bad70db465a
aminatkhamokova <150909090+aminatkhamokova@users.noreply.github.com>
2024-01-31 12:03:16 +0000

    Aminas brunch2 (#105)
    * privacy.anonymization added with test
    
    * privacy.anonymization added with test
    
    * reference changed
    
    * reference changed
    
    * blackbox attack added and privacy tutorial
    
    * privacy attack and tutorials added
    
    * init changed

---
+++ holisticai/privacy/attacks/__init__.py
@@ -0,0 +1,5 @@
+"""
+The :mod:holisticai.privacy.attacks module includes classification, regression, multiclass, recommender and clustering privacy metrics
+"""
+
+from ._BlackBoxAttack import BlackBoxAttack, append_if_not_empty
