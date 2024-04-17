d157023b0433d357d3696b9f4091c64e70b19251
aminatkhamokova <amina.tkhamokova@holisticai.com>
2024-04-17 12:26:59 +0100

    position parity changed to get fi dataframes, not indexes

--- holisticai/explainability/metrics/core/all_metrics.py
+++ holisticai/explainability/metrics/core/all_metrics.py
@@ -1,27 +1,24 @@
 from __future__ import annotations
 
-from typing import Iterable
-
 import numpy as np
-from numpy.typing import ArrayLike
+import pandas as pd
 
 from holisticai.explainability.metrics.global_importance._contrast_metrics import (
     importance_order_constrast,
-    importance_range_constrast,
 )
 
 
 def position_parity(
-    feature_importance_indexes: Iterable,
-    conditional_features_importance_indexes: list[Iterable],
+    feature_importance: pd.DataFrame,
+    conditional_feature_importance: list[pd.DataFrame],
 ):
     """
     Parameters
     ----------
-    feature_importance_indexes: Iterable
-        array with feature importance indexes
-    conditional_feature_importance_indexes: list[Iterable]
-        array with conditional feature importance indexes
+    feature_importance: pandas dataframe
+        dataframe with feature importances
+    conditional_feature_importance: list of dataframes
+        list of dataframes with conditional feature importances
 
     Returns
     -------
@@ -31,37 +28,9 @@ def position_parity(
     return np.mean(
         [
             importance_order_constrast(
-                feature_importance_indexes=feature_importance_indexes,
-                conditional_features_importance_indexes=i,
+                feature_importance_indexes=feature_importance.index,
+                conditional_features_importance_indexes=i.index,
             )
-            for i in conditional_features_importance_indexes
-        ]
-    )
-
-
-def rank_alignment(
-    feature_importance_indexes: Iterable,
-    conditional_features_importance_indexes: list[Iterable],
-):
-    """
-    Parameters
-    ----------
-    feature_importance_indexes: Iterable
-        array with feature importance indexes
-    conditional_feature_importance_indexes: list[Iterable]
-        array with conditional feature importance indexes
-
-    Returns
-    -------
-    float
-        rank alignment value
-    """
-    return np.mean(
-        [
-            importance_range_constrast(
-                feature_importance_indexes=feature_importance_indexes,
-                conditional_features_importance_indexes=i,
-            )
-            for i in conditional_features_importance_indexes
+            for i in conditional_feature_importance
         ]
     )
