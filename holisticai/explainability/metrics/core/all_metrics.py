fd5be2c436bab134a035f37ef3f968a27a104c99
aminatkhamokova <amina.tkhamokova@holisticai.com>
2024-04-17 15:07:50 +0100

    explainability ease added.

--- holisticai/explainability/metrics/core/all_metrics.py
+++ holisticai/explainability/metrics/core/all_metrics.py
@@ -7,6 +7,9 @@ from holisticai.explainability.metrics.global_importance._contrast_metrics impor
     importance_order_constrast,
     importance_range_constrast,
 )
+from holisticai.explainability.metrics.global_importance._explainability_level import (
+    compute_explainability_ease_score,
+)
 
 
 def position_parity(
@@ -63,3 +66,33 @@ def rank_alignment(
             for i in conditional_feature_importance
         ]
     )
+
+
+def explainability_ease(partial_dependence_list: list[dict]):
+    """
+    Parameters
+    ----------
+    partial_dependence_list: list[dict]
+        a list of dictionaries containing partial dependencies for each feature.
+        For multiclass classification, partial dependencies are computed for each class separately.
+        For binary classification, partial dependence is calculated only for the positive class,
+        resulting in a single dictionary in the list. Similarly, for regression, there's only one dictionary.
+
+    Returns
+    -------
+    float
+        explainability ease value, average explainability ease value for multiclass setting
+    """
+    if len(partial_dependence_list) == 1:
+        return compute_explainability_ease_score(
+            partial_dependence=partial_dependence_list[0]
+        )[0]
+    else:
+        return np.mean(
+            [
+                compute_explainability_ease_score(
+                    partial_dependence=partial_dependence
+                )[0]
+                for partial_dependence in partial_dependence_list
+            ]
+        )
