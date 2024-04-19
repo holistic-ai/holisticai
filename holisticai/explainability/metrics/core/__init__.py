d0cc56b4cadd1ac807a7183125b822dbbd8ebc86
aminatkhamokova <amina.tkhamokova@holisticai.com>
2024-04-19 13:55:16 +0100

    init changed

--- holisticai/explainability/metrics/core/__init__.py
+++ holisticai/explainability/metrics/core/__init__.py
@@ -2,11 +2,7 @@
 The :mod:`holisticai.explainability.metrics.core` module includes all explainability metrics.
 """
 
-from ._all_metrics import (
-    position_parity,
-    rank_alignment,
-    explainability_ease,
-)
+from ._all_metrics import explainability_ease, position_parity, rank_alignment
 
 # All explainability functions and classes
 __all__ = [
