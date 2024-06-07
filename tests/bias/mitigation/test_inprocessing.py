4b8ce46186ff6314ad3d7a9be43c3050e60bac2d
franklincf <fracarfer5@gmail.com>
2024-06-07 09:34:56 -0300

    tests: adding tests for recsys mitigators for in-, pre-, and post-processing methods

--- tests/bias/mitigation/test_inprocessing.py
+++ tests/bias/mitigation/test_inprocessing.py
@@ -1,14 +1,15 @@
 import warnings
-
+import numpy as np
 from sklearn.linear_model import LogisticRegression, LinearRegression
 
 from holisticai.pipeline import Pipeline
-from holisticai.metrics.bias import classification_bias_metrics, regression_bias_metrics, multiclass_bias_metrics
+from holisticai.metrics.bias import classification_bias_metrics, regression_bias_metrics, multiclass_bias_metrics, recommender_bias_metrics
 from tests.bias.mitigation.testing_utils.utils import (
     check_results,
     categorical_dataset,
     regression_dataset,
-    multiclass_dataset
+    multiclass_dataset,
+    recommender_dataset,
 )
 import pytest
 
@@ -35,7 +36,20 @@ def get_inprocessor(mitigator_name : MITIGATOR_NAME = "CalibratedEqualizedOdds",
         case "FairScoreClassifier":
             from holisticai.mitigation.bias import FairScoreClassifier
             return FairScoreClassifier(**parameters)
+        case "PopularityPropensityMF":
+            from holisticai.mitigation.bias import PopularityPropensityMF
+            return PopularityPropensityMF(**parameters)
+        case "BlindSpotAwareMF":
+            from holisticai.mitigation.bias import BlindSpotAwareMF
+            return BlindSpotAwareMF(**parameters)
+        case "FairRec":
+            from holisticai.mitigation.bias import FairRec
+            return FairRec(**parameters)
+        case "DebiasingLearningMF":
+            from holisticai.mitigation.bias import DebiasingLearningMF
+            return DebiasingLearningMF(**parameters)
     raise NotImplementedError
+
 
 @pytest.mark.parametrize("mitigator_name, mitigator_params, model_params", [
     ("FairScoreClassifier", {'objectives':'ab', 'constraints':{}}, {"random_state":42}),
@@ -65,6 +79,18 @@ def test_categorical_inprocessor(mitigator_name, mitigator_params, model_params,
 def test_regression_inprocessor(mitigator_name, mitigator_params, model_params, regression_dataset):
     metrics1 = run_inprocessing_categorical(regression_dataset, regression_bias_metrics, LinearRegression, mitigator_name, model_params, mitigator_params)
     metrics2 = run_inprocessing_catergorical_pipeline(regression_dataset, regression_bias_metrics, LinearRegression, mitigator_name, model_params, mitigator_params)
+    check_results(metrics1, metrics2)
+
+
+@pytest.mark.parametrize("mitigator_name, mitigator_params", [
+    ("PopularityPropensityMF", {'K':40, 'beta': 0.02, 'steps':10}),
+    ("BlindSpotAwareMF", {'K':40, 'beta': 0.02, 'steps':10}),
+    ("FairRec", {'rec_size':10, 'MMS_fraction':0.5}),
+    ("DebiasingLearningMF", {'K':40, 'normalization':'Vanilla', 'lamda':0.08, 'metric':'mse', 'bias_mode':'Regularized', 'seed':1}),
+])
+def test_recsys_inprocessor(mitigator_name, mitigator_params, recommender_dataset):
+    metrics1 = run_inprocessing_recsys(recommender_dataset, recommender_bias_metrics, mitigator_name, mitigator_params)
+    metrics2 = run_inprocessing_recsys_pipeline(recommender_dataset, recommender_bias_metrics, mitigator_name, mitigator_params)
     check_results(metrics1, metrics2)
 
 
@@ -106,3 +132,33 @@ def run_inprocessing_catergorical_pipeline(dataset, bias_metrics, estimator_clas
         return bias_metrics(test['group_a'],  y_pred, test['y'])
     else:
         return bias_metrics(test['group_a'], test['group_b'], y_pred, test['y'])
+
+
+def run_inprocessing_recsys(dataset, bias_metrics, mitigator_name, mitigator_params):
+    train = dataset['train']
+    inp = get_inprocessor(mitigator_name, mitigator_params)
+    data_matrix = train['data_pivot'].fillna(0).to_numpy()
+    inp.fit(data_matrix)
+    rankings  = inp.predict(data_matrix, top_n=10)
+    rankings = rankings.astype({'score':'float64'})
+    mat = rankings.pivot(columns='Y',index='X',values='score').replace(np.nan,0).to_numpy()
+
+    return bias_metrics(mat_pred=mat>0, metric_type='item_based')
+
+
+def run_inprocessing_recsys_pipeline(dataset, bias_metrics, mitigator_name, mitigator_params):
+    train = dataset['train']
+    inp = get_inprocessor(mitigator_name, mitigator_params)
+    data_matrix = train['data_pivot'].fillna(0).to_numpy()
+
+    pipeline = Pipeline(
+        steps=[
+            ("bm_inprocessing", inp),
+        ]
+    )
+    pipeline.fit(data_matrix)
+    rankings  = pipeline.predict(data_matrix, top_n=10)
+    rankings = rankings.astype({'score':'float64'})
+    mat = rankings.pivot(columns='Y',index='X',values='score').replace(np.nan,0).to_numpy()
+
+    return bias_metrics(mat_pred=mat>0, metric_type='item_based')
