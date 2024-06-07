27a755f19f56d04edb3929b0acede01d6e74134f
franklincf <fracarfer5@gmail.com>
2024-06-07 09:12:44 -0300

    feat: adding seed to allow reproducibility to synthetic generation ranking

--- src/holisticai/datasets/synthetic/recruitment.py
+++ src/holisticai/datasets/synthetic/recruitment.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 def generate_rankings(
-    M: Optional[int], k: Optional[int], p: Optional[float], return_p_attr=False
+    M: Optional[int], k: Optional[int], p: Optional[float], return_p_attr=False, seed: Optional[int] = 42
 ):
     """
     Generates M rankings of k elements (candidates) using Yang-Stoyanovich process
@@ -18,11 +18,17 @@ def generate_rankings(
         Number of elements should each ranking have
     p: float
         Probability that a candidate is protected
+    return_p_attr: bool
+        Whether to return protected attribute
+    seed: int
+        Random seed
 
-    Return
-    ------
+    Returns
+    -------
+    DataFrame, Tuple
         DataFrame or Tuple of DataFrames
     """
+    random.seed(seed)
     rankings = []
     for m in range(M):
         for i in range(k):
