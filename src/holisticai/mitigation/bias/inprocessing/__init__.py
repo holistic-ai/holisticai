3451206dad477f781135783d0b4a0fc52e6b6794
crismunoz <cristian.munoz@holisticai.com>
2024-06-17 11:05:45 -0300

    chore: remove import

--- src/holisticai/mitigation/bias/inprocessing/__init__.py
+++ src/holisticai/mitigation/bias/inprocessing/__init__.py
@@ -2,7 +2,6 @@
 from .exponentiated_gradient.transformer import ExponentiatedGradientReduction
 from .fair_k_center_clustering.transformer import FairKCenterClustering
 from .fair_k_mediam_clustering.transformer import FairKMedianClustering
-from .fair_scoring_classifier.transformer import FairScoreClassifier
 from .fairlet_clustering.transformer import FairletClustering
 from .grid_search.transformer import GridSearchReduction
 from .matrix_factorization.blind_spot_aware import BlindSpotAwareMF
