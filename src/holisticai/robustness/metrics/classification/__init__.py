847959a10d45d64329d0bed2a6c6c9cf78adcc78
franklincf <fracarfer5@gmail.com>
2024-07-24 07:48:38 -0300

    feat: combining binary classification robustness metrics in one file

--- src/holisticai/robustness/metrics/classification/__init__.py
+++ src/holisticai/robustness/metrics/classification/__init__.py
@@ -1,4 +1,6 @@
-from holisticai.robustness.metrics.classification._adversarial_accuracy import adversarial_accuracy
-from holisticai.robustness.metrics.classification._empirical_robustness import empirical_robustness
+from holisticai.robustness.metrics.classification._binary_classification import (
+    adversarial_accuracy,
+    empirical_robustness,
+)
 
 __all__ = ["adversarial_accuracy", "empirical_robustness"]
