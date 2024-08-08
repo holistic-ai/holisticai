b3defafab82493f1c9bff0ed5f3c15c407d5d8c6
franklincf <fracarfer5@gmail.com>
2024-08-08 11:59:50 -0300

    feat: adding robustness hopskipjump and zoo attackers

---
+++ src/holisticai/robustness/attackers/__init__.py
@@ -0,0 +1,4 @@
+from holisticai.robustness.attackers.classification.hop_skip_jump import HopSkipJump
+from holisticai.robustness.attackers.classification.zeroth_order_optimization import ZooAttack
+
+__all__ = ["HopSkipJump", "ZooAttack"]
