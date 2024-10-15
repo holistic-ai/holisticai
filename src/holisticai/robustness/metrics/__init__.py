a405e9f8b8eaf9d17ef71a95211de6d3201bc911
Andre <andrelfnovaes@gmail.com>
2024-10-15 10:55:37 -0300

    feature: update dataset_shift module

--- src/holisticai/robustness/metrics/__init__.py
+++ src/holisticai/robustness/metrics/__init__.py
@@ -1,3 +1,14 @@
 from holisticai.robustness.metrics.classification import adversarial_accuracy, empirical_robustness
+from holisticai.robustness.metrics.dataset_shift._accuracy_degradation_profile import (
+    accuracy_degradation_factor,
+    accuracy_degradation_profile,
+    pre_process_data,
+)
 
-__all__ = ["adversarial_accuracy", "empirical_robustness"]
+__all__ = [
+    "adversarial_accuracy",
+    "empirical_robustness",
+    "accuracy_degradation_profile",
+    "accuracy_degradation_factor",
+    "pre_process_data",
+]
