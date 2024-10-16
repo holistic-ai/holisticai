922d3417da63cee459d320af61c11cde12ddda82
andrelfnovaes <40208986+andrelfnovaes@users.noreply.github.com>
2024-10-16 10:59:31 -0300

    feature: update dataset_shift module (#248)
    * feature: update dataset_shift module
    
    * feature: update dataset_shift module
    
    * feature: update dataset_shift module
    
    * feature: update dataset_shift module
    
    * feature: update dataset_shift module
    
    * fix: update dataset_shift code and plots
    
    * fix: update dataset_shift code and plots
    
    * fix: update dataset_shift code and plots
    
    * fix: percent_degradation to ADP nomenclature

--- src/holisticai/robustness/metrics/dataset_shift/__init__.py
+++ src/holisticai/robustness/metrics/dataset_shift/__init__.py
@@ -1,14 +1,15 @@
-from holisticai.robustness.metrics.classification import adversarial_accuracy, empirical_robustness
+# Documentation
+
+# Import modules
 from holisticai.robustness.metrics.dataset_shift._accuracy_degradation_profile import (
     accuracy_degradation_factor,
     accuracy_degradation_profile,
     pre_process_data,
 )
 
+# Allow functions and classes
 __all__ = [
-    "adversarial_accuracy",
-    "empirical_robustness",
-    "accuracy_degradation_profile",
     "accuracy_degradation_factor",
+    "accuracy_degradation_profile",
     "pre_process_data",
 ]
