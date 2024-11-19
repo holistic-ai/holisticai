ce6a83c98e5ad96f88ec5aa1aac8048d685e652f
Cristian Muñoz <cristian.munoz@holisticai.com>
2024-11-19 10:11:50 -0500

    Update Main Branch (#279)
    * chore: update files for new gitflow (#227)
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 4.1.7 to 4.2.2.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v4.1.7...v4.2.2)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps): bump actions/setup-python from 5.2.0 to 5.3.0 (#251)
    
    Bumps [actions/setup-python](https://github.com/actions/setup-python) from 5.2.0 to 5.3.0.
    - [Release notes](https://github.com/actions/setup-python/releases)
    - [Commits](https://github.com/actions/setup-python/compare/v5.2.0...v5.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: actions/setup-python
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * feat: New Robustness Regression Attacker: Ridge Poissoner (#241)
    
    ---------
    
    Co-authored-by: Cristian Muñoz <cristian.munoz@holisticai.com>
    
    * feat: new xai metrics based on globa feature importance, local feature importance and surrogate  (#255)
    ---------
    
    Co-authored-by: Kleyton da Costa <kleyton.vsc@gmail.com>
    
    * chore: Improve Accuracy Degradation Profile Documentation  (#249)
    
    ---------
    
    Co-authored-by: crismunoz <crisstrink@gmail.com>
    
    * docs:update tutorials (#257)
    
    ---------
    
    Co-authored-by: crismunoz <crisstrink@gmail.com>
    
    * add: bias tradeoff example (#266)
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Kleyton da Costa <44351707+Kleyt0n@users.noreply.github.com>
    Co-authored-by: andrelfnovaes <40208986+andrelfnovaes@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Franklin Cardenoso <44931423+fracarfer5@users.noreply.github.com>
    Co-authored-by: Kleyton da Costa <kleyton.vsc@gmail.com>

--- src/holisticai/datasets/_dataloaders.py
+++ src/holisticai/datasets/_dataloaders.py
@@ -4,9 +4,6 @@ from os.path import expanduser, join
 
 import pandas as pd
 
-# sklearn imports
-from sklearn.datasets import fetch_openml
-
 
 def get_data_home(data_home=None):
     """
@@ -34,151 +31,50 @@ def get_data_home(data_home=None):
     return data_home
 
 
-def load_openml(name, version, data_home=None, return_X_y=False, as_frame=True):
+def load_hai_datasets(dataset_name, data_home=None):
     """
-    Generic function to load datasets from OpenML.
+    Generic function to load datasets from holisticai datasets repository.
+
+    Available datasets:
+    - adult
+    - law_school
+    - student
+    - lastfm
+    - us_crime
+    - clinical_records
+    - acsincome
+    - acspublic
+    - compass_two_year_recid
+    - compass_is_recid
+    - german_credit
+    - bank_marketing
+    - census_kdd
+    - diabetes
+    - mw_small
+    - mw_medium
 
     Parameters
     ----------
     name : str
-        Name of the dataset on OpenML.
+        Name of the dataset.
     version : int
         Version of the dataset.
     data_home : str, optional
         The directory to which the data is downloaded. If None, we download
         to the default data home directory.
-    return_X_y : bool, default=False
-        If True, returns ``(data, target)`` instead of a Bunch object.
-    as_frame : bool, default=True
-        If True, the data is a pandas DataFrame including columns with
-        appropriate dtypes (numeric).
-
     Returns
     -------
-    data : sklearn.utils.Bunch or tuple
-        Dataset object or (data, target) tuple if return_X_y is True.
+    data : pd.DataFrame
+        The dataset from holisticai datasets.
+
+    References
+    ----------
+    .. [1] https://huggingface.co/datasets/holistic-ai/holisticai-datasets
+
     """
     if data_home is None:
         data_home = get_data_home()
 
-    return fetch_openml(
-        name=name,
-        version=version,
-        data_home=data_home,
-        return_X_y=return_X_y,
-        as_frame=as_frame,
+    return pd.read_parquet(
+        f"https://huggingface.co/datasets/holistic-ai/holisticai-datasets/resolve/main/data/{dataset_name}/{dataset_name}_dataset.parquet"
     )
-
-
-def load_student(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("UCI-student-performance-mat", 1, data_home, return_X_y, as_frame)
-
-
-def load_adult(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("adult", 2, data_home, return_X_y, as_frame)
-
-
-def load_law_school(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("law-school-admission-bianry", 1, data_home, return_X_y, as_frame)
-
-
-def load_last_fm(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("LastFM_dataset", 1, data_home, return_X_y, as_frame)
-
-
-def load_us_crime(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("us_crime", 1, data_home, return_X_y, as_frame)
-
-
-def load_heart(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("heart-failure", 1, data_home, return_X_y, as_frame)
-
-
-def load_german_credit(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("German-Credit-Risk-with-Target", 1, data_home, return_X_y, as_frame)
-
-
-def load_census_kdd(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("Census-Income-KDD", 3, data_home, return_X_y, as_frame)
-
-
-def load_bank_marketing(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("bank-marketing", 9, data_home, return_X_y, as_frame)
-
-
-def load_compas_two_year_recid():
-    data = pd.read_csv(
-        "https://raw.githubusercontent.com/propublica/compas-analysis/master/compas-scores-two-years.csv"
-    )
-    df = data.loc[
-        (data["days_b_screening_arrest"] <= 30)
-        & (data["days_b_screening_arrest"] >= -30)
-        & (data["is_recid"] != -1)
-        & (data["c_charge_degree"] != "O")
-        & (data["score_text"] != "N/A"),
-        [
-            "age",
-            "c_charge_degree",
-            "race",
-            "age_cat",
-            "score_text",
-            "sex",
-            "priors_count",
-            "days_b_screening_arrest",
-            "decile_score",
-            "juv_fel_count",
-            "juv_misd_count",
-            "juv_other_count",
-            "v_type_of_assessment",
-            "c_days_from_compas",
-            "v_score_text",
-            "v_decile_score",
-            "two_year_recid",
-        ],
-    ]
-    return df
-
-
-def load_compas_is_recid():
-    data = pd.read_csv(
-        "https://raw.githubusercontent.com/propublica/compas-analysis/master/compas-scores-two-years.csv"
-    )
-    df = data.loc[
-        (data["days_b_screening_arrest"] <= 30)
-        & (data["days_b_screening_arrest"] >= -30)
-        & (data["is_recid"] != -1)
-        & (data["c_charge_degree"] != "O")
-        & (data["score_text"] != "N/A"),
-        [
-            "age",
-            "c_charge_degree",
-            "race",
-            "age_cat",
-            "score_text",
-            "sex",
-            "priors_count",
-            "days_b_screening_arrest",
-            "decile_score",
-            "juv_fel_count",
-            "juv_misd_count",
-            "juv_other_count",
-            "v_type_of_assessment",
-            "c_days_from_compas",
-            "v_score_text",
-            "v_decile_score",
-            "is_recid",
-        ],
-    ]
-    return df
-
-
-def load_diabetes(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("Diabetes-130-Hospitals_(Fairlearn)", 2, data_home, return_X_y, as_frame)
-
-
-def load_acsincome(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("ACSIncome", 3, data_home, return_X_y, as_frame)
-
-
-def load_acspublic(data_home=None, return_X_y=False, as_frame=True):
-    return load_openml("ACSPublicCoverage", 2, data_home, return_X_y, as_frame)
